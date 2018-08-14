15d9649d53adeeaca95b261996b4aaf9da1a0e58
mirmik <mirmikns@yandex.ru>
2018-08-14 08:38:23 +0300

    a

--- crow/defs.h
+++ crow/defs.h
@@ -2,12 +2,10 @@
 	@file
 */
 
-#ifndef G1_INDEXES_H
-#define G1_INDEXES_H
+#ifndef CROW_DEFS_H
+#define CROW_DEFS_H
 
 #define G1_UDPGATE 12
-#define G1_TESTGATE 99
-#define G1_SELFGATE 100
 
 #define G1_ACK_TYPE 0
 #define G1_ACK21_TYPE 1
