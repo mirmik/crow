beceb76fd9bb78d49d14545a724bb90a99b45628
mirmik <mirmikns@yandex.ru>
2018-12-25 20:23:46 +0500

    a

--- pytest/test.py
+++ pytest/test.py
@@ -4,19 +4,14 @@
 import sys
 import pycrow
 
-pycrow.diagnostic(pack=True, live=True)
+pycrow.diagnostic(pack=True)
 
-@pycrow.subscribe_handler("mirmik")
+@pycrow.subscribe_handler("pulse")
 def subscribe_handler(packet):
 	print("ttt", packet.data())
 
-@pycrow.subscribe_handler("log")
-def subscribe_handler(packet):
-	print("log", packet.data())
-
 pycrow.set_crowker(".12.109.173.108.206:10009")
-pycrow.subscribe("mirmik", ack=2, rack=2)
-pycrow.subscribe("log")
+pycrow.subscribe("pulse", ack=2, rack=2)
 
 pycrow.create_udpgate(12, 10009)
 pycrow.spin()
