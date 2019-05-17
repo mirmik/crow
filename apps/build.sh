d7ea5ef961662252f52150a00708cfd47d22ac11
mirmik <mirmikns@yandex.ru>
2019-05-17 14:55:57 +0300

    'g'

--- apps/build.sh
+++ apps/build.sh
@@ -3,11 +3,5 @@ export PWD0=$PWD
 cd $PWD0/crowker
 ./make.py
 
-cd $PWD0/crow_publish
-./make.py
-
-cd $PWD0/crow_subscribe
-./make.py
-
 cd $PWD0/ctrans
 ./make.py
--- apps/clean.sh
+++ apps/clean.sh
@@ -3,11 +3,5 @@ export PWD0=$PWD
 cd $PWD0/crowker
 ./make.py clean
 
-cd $PWD0/crow_publish
-./make.py clean
-
-cd $PWD0/crow_subscribe
-./make.py clean
-
 cd $PWD0/ctrans
 ./make.py clean
