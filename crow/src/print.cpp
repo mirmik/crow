e0e4f292900df922d43f8c2a9b412986c6f4053d
mirmik <mirmikns@yandex.ru>
2019-05-24 10:55:49 +0300

    'print'

--- crow/src/print.cpp
+++ crow/src/print.cpp
@@ -5,38 +5,11 @@
 #include <nos/fprint.h>
 #include <nos/print.h>
 
-/*void crow_print_to(igris::io::ostream& out, crow::packet* pack) {
-	igris::fprint_to(out, "("
-		"qos:{}, "
-		"ack:{}, "
-		"alen:{}, "
-		"type:{}, "
-		"addr:{}, "
-		"stg:{}, "
-		"data:{}, "
-		"released:{}"
-		")",
-		pack->header.qos,
-		(uint8_t)pack->header.f.ack,
-		pack->header.alen,
-		(uint8_t)pack->header.f.type,
-		igris::hexascii_encode((const uint8_t*)pack->addrptr(),
-pack->header.alen), pack->header.stg, igris::dstring(pack->data()), pack->flags
-	);
-}
-
-void crow_print(crow::packet* pack) {
-	crow_print_to(*igris::standart_output, pack);
-}
-
-void crow_println(crow::packet* pack) {
-	crow_print_to(*igris::standart_output, pack);
-	igris::print_to(*igris::standart_output, "\n");
-}*/
-
 void crow::diagnostic(const char *notation, crow::packet *pack)
 {
-	nos::fprintln(
+	bool postfix_points = pack->rawdata().size() > 16;
+
+	nos::fprint(
 		"{}: ("
 		"qos:{}, "
 		"ack:{}, "
@@ -46,8 +19,7 @@ void crow::diagnostic(const char *notation, crow::packet *pack)
 		"addr:{}, "
 		"stg:{}, "
 		"dlen:{}, "
-		"data:{}"
-		")",
+		"data:{}",
 		notation, pack->header.qos, (uint8_t)pack->header.f.ack,
 		(uint8_t)pack->header.alen, (uint16_t)pack->header.flen,
 		(uint8_t)pack->header.f.type,
@@ -55,4 +27,9 @@ void crow::diagnostic(const char *notation, crow::packet *pack)
 		pack->header.stg,
 		pack->rawdata().size(),
 		igris::dstring(pack->rawdata().data(), pack->rawdata().size() > 16 ? 16 : pack->rawdata().size()));
+
+		if (postfix_points) 
+			nos::println("...)");
+		else
+			nos::println(")");
 }
