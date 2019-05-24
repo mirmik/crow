d4f7cc77292f7438cad42113ea592f1fcd3da6b3
mirmik <mirmikns@yandex.ru>
2019-05-24 10:49:19 +0300

    'subscribe fix'

--- apps/ctrans/main.cpp
+++ apps/ctrans/main.cpp
@@ -454,8 +454,8 @@ int main(int argc, char *argv[])
 	{
 		std::thread([](){
 			while(1) {
-				std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 				crow::subscribe(addr, addrsize, theme.c_str(), qos, ackquant, qos, ackquant);
+				std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 			}
 		}).detach();
 	}
