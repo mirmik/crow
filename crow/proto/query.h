b114f6eee4fbec9a966856bfbec904b8a91a6beb
mirmik <mirmikns@yandex.ru>
2020-03-26 18:08:56 +0300

    'a'

---
+++ crow/proto/query.h
@@ -0,0 +1,11 @@
+#ifndef CROW_QUERY_H
+#define CROW_QUERY_H
+
+#include <crow/proto/node.h>
+
+namespace crow 
+{
+
+}
+
+#endif
---
+++ crow/proto/request.h
@@ -0,0 +1,14 @@
+#ifndef CROW_REQUEST_API_H
+#define CROW_REQUEST_API_H
+
+#include <crow/proto/node.h>
+
+namespace crow 
+{
+	class msgbox : public crow::node
+	{
+
+	};
+}
+
+#endif
