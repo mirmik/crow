3ee92ebe219fbcdb14cbda5e0c57d6161a7eb851
mirmik <mirmikns@yandex.ru>
2021-03-03 11:23:09 +0300

    'change ignoring'

---
+++ apps/crowker/control_node.cpp
@@ -0,0 +1,34 @@
+#include <crow/proto/rpc.h>
+#include "brocker.h"
+#include <string>
+
+crow::rpc_node rpc;
+
+int hello() 
+{
+	return 42;
+}
+
+std::string get_themes() 
+{
+	std::string ret;
+
+	auto& themes = brocker::themes;
+
+	for (auto& a : themes) 
+	{
+		std::string s = nos::format("name:{} subs:{} lasta:{} lastp:{}\n", 
+			a.first, a.second.subs.size(), a.second.timestamp_activity, a.second.timestamp_publish);
+		ret.append(s);
+	}
+
+	return ret;
+}
+
+void init_control_node() 
+{
+	rpc.bind();
+
+	rpc.add_delegate("hello", igris::make_delegate(hello));
+	rpc.add_delegate("themes", igris::make_delegate(get_themes));
+}
