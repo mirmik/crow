7d8e81702c3ca4bf4cc188b3fa66c0db454f6a06
mirmik <mirmikns@yandex.ru>
2021-10-07 11:36:53 +0300

    'types'

--- crow/nodes/pubsub_defs.h
+++ crow/nodes/pubsub_defs.h
@@ -17,37 +17,37 @@ namespace crow
     struct pubsub_subheader : public node_subheader
     {
         PubSubTypes type;
-    };
+    } __attribute__((packed));
 
     struct publish_subheader : public pubsub_subheader
     {
-        int thmsize;
-        int datsize;
+        uint8_t thmsize;
+        uint16_t datsize;
         igris::buffer theme() { return {(char *)(this + 1), thmsize}; }
         igris::buffer message()
         {
             return {(char *)(this + 1) + thmsize, datsize};
         }
-    };
+    } __attribute__((packed));
 
     struct subscribe_subheader : public pubsub_subheader
     {
-        int rqos;
-        int rackquant;
-        int keepalive;
-        int thmsize;
+        uint8_t rqos;
+        uint16_t rackquant;
+        uint16_t keepalive;
+        uint8_t thmsize;
         igris::buffer theme() { return {(char *)(this + 1), thmsize}; }
-    };
+    } __attribute__((packed));
 
     struct consume_subheader : public pubsub_subheader
     {
         int datsize;
         igris::buffer message() { return {(char *)(this + 1), datsize}; }
-    };
+    } __attribute__((packed));
 
     struct publish_and_subscribe_subheader : public pubsub_subheader
     {
-    };
+    } __attribute__((packed));
 }
 
 #endif
