ee39c20c9f60d79ee28e13bec4f29ab601405e36
mirmik <mirmikns@yandex.ru>
2021-10-08 16:03:49 +0300

    'a'

--- crow/nodes/publisher_node.h
+++ crow/nodes/publisher_node.h
@@ -7,7 +7,7 @@ namespace crow
 {
     class publisher_node : public crow::node
     {
-          protected:
+       protected:
         igris::buffer theme;
         crow::hostaddr_view crowker_addr;
         int crowker_node = CROWKER_SERVICE_BROCKER_NODE_NO;
@@ -15,13 +15,13 @@ namespace crow
         int qos = 2;
         int ackquant = 50;
 
-          public:
+       public:
         void publish(igris::buffer data);
         void publish(crow::hostaddr_view addr, int crowker_node,
                      igris::buffer theme, igris::buffer data, int qos,
                      int ackquant);
 
-          private:
+       private:
         void incoming_packet(crow_packet *pack) override
         {
             crow::release(pack);
