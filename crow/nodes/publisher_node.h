a6e03f9eef8358350c407d0bc4106404d12b9856
mirmik <mirmikns@yandex.ru>
2021-12-07 21:10:20 +0300

    'a'

--- crow/nodes/publisher_node.h
+++ crow/nodes/publisher_node.h
@@ -18,11 +18,6 @@ namespace crow
 
     public:
         publisher_node() = default;
-        publisher_node(const publisher_node &) = default;
-        publisher_node(publisher_node &&) = default;
-
-        publisher_node &operator=(const publisher_node &) = default;
-        publisher_node &operator=(publisher_node &&) = default;
 
         publisher_node(crow::hostaddr_view crowker_addr, int crowker_node,
                        igris::buffer theme);
