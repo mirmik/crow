afd14eb5327564e471b9ee2a9bf67423213660db
mirmik <mirmikns@yandex.ru>
2022-05-22 22:50:43 +0300

    'a'

--- tests/make.py
+++ tests/make.py
@@ -19,7 +19,7 @@ application("runtests",
 
 	cxxstd="gnu++17",
 	ccstd="c11",
-	cxx_flags = "-g -Werror=all -Werror=pedantic -Wno-vla-extension -Weffc++",
+	cxx_flags = "-g -Werror=all -Werror=pedantic -Weffc++",
 	cc_flags = "-g -Werror=all -Werror=pedantic",
 
 	include_paths = ["."],
