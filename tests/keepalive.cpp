c098a2346bd2bec051482229140dd38a628b9853
mirmik <mirmikns@yandex.ru>
2022-09-15 17:45:04 +0300

    simplify test

--- tests/keepalive.cpp
+++ tests/keepalive.cpp
@@ -1,40 +1,41 @@
-#include <doctest/doctest.h>
-#include <crow/tower.h>
+#include <chrono>
 #include <crow/proto/node.h>
+#include <crow/tower.h>
+#include <doctest/doctest.h>
 #include <iostream>
 #include <thread>
-#include <chrono>
 
 static int a = 0;
 static int b = 0;
 
-class test_keepalive_node : public  crow::node, public crow::alived_object 
+class test_keepalive_node : public crow::node, public crow::alived_object
 {
-	int& ptr;
+    int &ptr;
+
 public:
-	test_keepalive_node(int& ptr) : ptr(ptr) {} 
+    test_keepalive_node(int &ptr) : ptr(ptr) {}
 
-	void keepalive_handle() override 
-	{
-		ptr++;
-	}
+    void keepalive_handle() override
+    {
+        ptr++;
+    }
 
-	void incoming_packet(crow::packet*) override {}
+    void incoming_packet(crow::packet *) override {}
 };
 
-TEST_CASE("keepalive") 
+TEST_CASE("keepalive")
 {
-	test_keepalive_node an(a);
-	test_keepalive_node bn(b);
-	an.install_keepalive(10);
-	bn.install_keepalive(20);
+    test_keepalive_node an(a);
+    test_keepalive_node bn(b);
+    an.install_keepalive(10);
+    bn.install_keepalive(20);
 
-	int64_t start = igris::millis();
-	while(igris::millis() - start < 45) 
-	{
-		crow::onestep();
-	}
+    int64_t start = igris::millis();
+    while (igris::millis() - start < 41)
+    {
+        crow::onestep();
+    }
 
-	CHECK_EQ(a, 5);
-	CHECK_EQ(b, 3);
+    CHECK_EQ(a, 5);
+    CHECK_EQ(b, 3);
 }
