7090cbcbf9ef3de11d04d62bc96800b07293886d
mirmik <mirmikns@yandex.ru>
2023-03-01 12:10:40 +0300

    'add test for nospublisher'

--- crow/nodes/nospublisher.h
+++ crow/nodes/nospublisher.h
@@ -8,9 +8,10 @@ namespace crow
 {
     class nospublisher : public crow::publisher_node, public nos::ostream
     {
-      public:
+    public:
         nospublisher() = default;
-        nospublisher(crow::hostaddr_view crowker_addr, int crowker_node,
+        nospublisher(crow::hostaddr_view crowker_addr,
+                     int crowker_node,
                      igris::buffer theme)
             : publisher_node(crowker_addr, crowker_node, theme)
         {
@@ -23,7 +24,8 @@ namespace crow
 
         nospublisher(const nospublisher &) = default;
 
-        int write(const void *ptr, size_t sz) override
+        nos::expected<size_t, nos::output_error> write(const void *ptr,
+                                                       size_t sz) override
         {
             crow::publisher_node::publish({ptr, sz});
             return sz;
